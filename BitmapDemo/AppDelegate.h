a501884bcabb2846bff2caab03b095a2389906ce
ycong1992 <709932492@qq.com>
2020-03-11 11:38:07 +0800

    Initial Commit

---
+++ BitmapDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BitmapDemo
+//
+//  Created by SZOeasy on 2020/3/11.
+//  Copyright © 2020 ycong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BitmapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BitmapDemo
+//
+//  Created by SZOeasy on 2020/3/11.
+//  Copyright © 2020 ycong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
